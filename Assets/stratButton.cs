f7e35aaf8cb5723f14b6e9be7107b6079a19b40c
kdix-23-240 <akihide.3kami@gmail.com>
2025-10-22 19:29:55 +0900

    タイトルのイメージを追加

--- Assets/stratButton.cs
+++ Assets/stratButton.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 // ライブラリの追加
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-public class startButton : MonoBehaviour {	
-　// 始まった時に実行する関数	
-　void Start () {
-　	gameObject.GetComponent<Button>().onClick.AddListener(StartGame);
- } 
-　// StartGame関数 
-　void StartGame() {
- 	// GameSceneをロード 
- 	SceneManager.LoadScene("SampleScene"); 
- }
+public class startButton : MonoBehaviour
+{
+    // 始まった時に実行する関数	
+    void Start()
+    {
+        gameObject.GetComponent<Button>().onClick.AddListener(StartGame);
+    }
+    // StartGame関数 
+    void StartGame()
+    {
+        // GameSceneをロード 
+        SceneManager.LoadScene("SampleScene");
+    }
 }
